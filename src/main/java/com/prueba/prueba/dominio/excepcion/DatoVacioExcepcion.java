aaa6f641b72fc7a7f93dd0fd9034eb85b7e4be89
pivenwar <46358544+pivenwar@users.noreply.github.com>
2021-06-25 17:22:26 -0500

    Delete src directory

--- src/main/java/com/prueba/prueba/dominio/excepcion/DatoVacioExcepcion.java
+++
@@ -1,9 +0,0 @@
-package com.prueba.prueba.dominio.excepcion;
-
-
-public class DatoVacioExcepcion extends DominioExcepcion {
-
-    public DatoVacioExcepcion(String mensaje) {
-        super(mensaje);
-    }
-}
--- src/main/java/com/prueba/prueba/dominio/excepcion/DominioExcepcion.java
+++
@@ -1,9 +0,0 @@
-package com.prueba.prueba.dominio.excepcion;
-
-public class DominioExcepcion extends RuntimeException {
-
-    public DominioExcepcion(String mensaje) {
-        super(mensaje);
-    }
-
-}
